26e9537f0fb179d4514f2177219a6e02b27806ec
Demo <51279632+Demo4889@users.noreply.github.com>
2020-05-04 21:11:01 -0400

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'ESX Repairkit'
 version '2.0'
@@ -6,17 +8,13 @@ version '2.0'
 client_scripts {
 	'@es_extended/locale.lua',
 	'client/main.lua',
-	'locales/fr.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
 	'config.lua'
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
 	'server/main.lua',
-	'locales/fr.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
 	'config.lua'
 }
