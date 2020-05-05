f7f9d978a27300d869cedb81a02adf39c8f090c3
Demo <51279632+Demo4889@users.noreply.github.com>
2020-05-04 21:17:06 -0400

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,4 @@
 Locales ['en'] = {
-	['used_kit']					= 'you used ~y~x1 ~b~Repairkit',
 	['must_be_outside']				= 'you must be outside of the vehicle!',
 	['no_vehicle_nearby']			= 'there is no ~r~vehicle ~w~nearby',
 	['finished_repair_engine']		= 'you repaired the vehicles ~g~engine~s~!',
